33fe5de27c91912939c90272d39e91e72cfe84ba
Kate Ramsey <kateramsey@live.com>
2016-03-02 15:28:23 -0600

    Add logic to rotate players

--- PigDice/Program.cs
+++ PigDice/Program.cs
@@ -25,49 +25,57 @@ namespace PigDice
 
             Player[] Players = new Player[NumOfPlayers];
 
-        while(GameStillGoing)
-            { 
-            while (PlayerTurn)
+            while(GameStillGoing)
             {
-                CurrentRoll = DiceRoll();
-                Console.WriteLine($"The Dice Roll was {CurrentRoll}");
-                if (CurrentRoll == 1)
+                while (PlayerTurn)
                 {
-                    PlayerTurn = false;
+                    CurrentRoll = DiceRoll();
+                    Console.WriteLine($"The Dice Roll was {CurrentRoll}");
+                    if (CurrentRoll == 1)
+                    {
+                        PlayerTurn = false;
+                    }
+                    else
+                    {
+                        TurnScore = TurnScore + CurrentRoll;
+
+                        if (Players[CurrentPlayerNumber].Score + TurnScore >= 100)
+                        {
+                            Console.WriteLine("Wow, you have more than 100 points! You Win!!");
+                            GameStillGoing = false;
+                            break;
+                        }
+
+                        Console.WriteLine($"Your Score this turn is currently {TurnScore}");
+                        Console.Write($"Would you like to bank your {TurnScore} or roll again? ");
+                        Console.WriteLine("Please enter B or R");
+                        BankOrRoll = char.Parse(Console.ReadLine());
+                        while (BankOrRoll != 'B' && BankOrRoll != 'b' && BankOrRoll != 'R' && BankOrRoll != 'r')
+                        {
+                            Console.WriteLine("Please enter B or R");
+                            BankOrRoll = char.Parse(Console.ReadLine());
+                        }
+
+                        if (BankOrRoll == 'B' || BankOrRoll == 'b')
+                        {
+                            Players[CurrentPlayerNumber].Score += TurnScore;
+                            PlayerTurn = false;
+                        }
+
+                    }
+                }
+                Console.WriteLine("It's not your turn anymore");
+                //rotate player
+                if(CurrentPlayerNumber == NumOfPlayers - 1)
+                {
+                    CurrentPlayerNumber = 0;
                 }
                 else
                 {
-                    TurnScore = TurnScore + CurrentRoll;
-
-                    //test win
-                    //if win display win text and break
-                    if (Players[CurrentPlayerNumber].Score + TurnScore >= 100)
-                    {
-                        Console.WriteLine("Wow, you have more than 100 points! You Win!!");
-                        break;
-                    }
-
-                    Console.WriteLine($"Your Score this turn is currently {TurnScore}");
-                    Console.Write($"Would you like to bank your {TurnScore} or roll again? ");
-                    Console.WriteLine("Please enter B or R");
-                    BankOrRoll = char.Parse(Console.ReadLine());
-                    while (BankOrRoll != 'B' && BankOrRoll != 'b' && BankOrRoll != 'R' && BankOrRoll != 'r')
-                    {
-                        Console.WriteLine("Please enter B or R");
-                        BankOrRoll = char.Parse(Console.ReadLine());
-                    }
-
-                    if (BankOrRoll == 'B' || BankOrRoll == 'b')
-                    {
-                        Players[CurrentPlayerNumber].Score += TurnScore;
-                        PlayerTurn = false;
-                    }
-
+                    CurrentPlayerNumber++;
                 }
+                PlayerTurn = true;
             }
-            Console.WriteLine("It's not your turn anymore");
-            //rotate player
-        }
 
 
             Console.ReadLine();
